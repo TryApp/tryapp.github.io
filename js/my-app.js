e7483a6d9f6f0acdc2ada98966d30d202e7ad0fc
Vineet Choudhary <vineetchoudhary@live.in>
2018-06-10 22:00:22 +0530

    added toast for compatibility

--- js/my-app.js
+++ js/my-app.js
@@ -141,24 +141,27 @@ if (qrStr) {
             if (myApp.device.ios) {
               if ((myApp.device.iphone || myApp.device.ipod)) {
                 if (!supporteddevice.toLowerCase().includes('iphone')) {
-                  message += appTitle + ' not compatible with ' + iPhone ? 'iPhone.' : 'iPod';
+                  message = '"'+ appTitle + '" not compatible with ' + iPhone ? 'iPhone.' : 'iPod.';
                 }
               } else if (myApp.device.ipad) {
                 if (!supporteddevice.toLowerCase().includes('ipad')) {
-                  message += appTitle + ' not compatible with iPad. ';
+                  message = '"'+ appTitle + '" not compatible with "iPad." ';
                 }
               }
-              if (myApp.device.osVersion >= minOSVersion) {
-                message += appTitle + ' only compatible with iOS ' + minOSVersion + 'or higher.';
+              if (myApp.device.osVersion <= minOSVersion) {
+                message = '"' + appTitle + '" only compatible with iOS ' + minOSVersion + ' or higher.';
               }
             } else {
-              message += 'Open this page on your iOS device to install the' + appTitle + '. ';
+              message += 'Open this page on your iOS device to install the "' + appTitle + '".';
             }
             if (message.length > 0) {
-              myApp.toast.create({
-                text: message,
-                closeButton: true,
-              }).show();
+              myApp.addNotification({
+                message: message,
+                title: "AppBox",
+                button: {
+                    text: 'Ok',
+                },
+            });
             }
           } else {
             //hide hide more details for old builds
