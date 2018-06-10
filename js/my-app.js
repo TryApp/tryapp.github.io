ebaee4a80eac794fa7abb4f744648c0faa7f8dac
Vineet Choudhary <vineetchoudhary@live.in>
2018-06-10 14:13:00 +0530

    reverted background image instyle

--- js/my-app.js
+++ js/my-app.js
@@ -83,7 +83,7 @@ if (qrStr) {
           //app icon
           var appIcon = response.latestVersion.icon;
           document.getElementById('appIconItem').hidden = (appIcon == null);
-          document.getElementById('appIcon').style.backgroundImage = appIcon;
+          document.getElementById('appIcon').style.backgroundImage = "url('" + appIcon + "')";
 
           //bundle identifier
           var appIdentifier = response.latestVersion.identifier;
